fd9b7574ab239588abc1458974ec049f66654c51
Oseias <oseias@obi.tec.br>
2025-03-24 22:37:50 -0300

    Code revision.

--- prisma/migrations/20241123141612_init/migration.sql
+++ prisma/migrations/20241123141612_init/migration.sql
@@ -7,3 +7,28 @@ CREATE TABLE `User` (
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+CREATE TABLE "tecnologies" (
+   `id` INTEGER NOT NULL AUTO_INCREMENT,
+   `name` VARCHAR(191) NOT NULL,
+   `description` VARCHAR(191) NOT NULL,
+   `image` VARCHAR(191) NOT NULL,
+   `emphasis` BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "tecnologies_pkey" PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+CREATE TABLE "projects" (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+   `name` VARCHAR(191) NOT NULL,
+   `description` VARCHAR(191) NOT NULL,
+   `image` VARCHAR(191) NOT NULL,
+   `level` INTEGER NOT NULL,
+   `type` VARCHAR(191) NOT NULL,
+   `emphasis` BOOLEAN NOT NULL DEFAULT false,
+   `repository` VARCHAR(191) NOT NULL,
+
+    CONSTRAINT "projetos_pkey" PRIMARY KEY ("id")
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
