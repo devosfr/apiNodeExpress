55afc9b439450187cc4f4defb7e4cf43936ffc74
Oseias <oseias@obi.tec.br>
2025-04-10 17:33:30 -0300

    Adding cors solution.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+import cors from 'cors';
 const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+
+app.use(cors()); // Permite qualquer origem
+
+// ou, para permitir apenas o seu front-end:
+app.use(cors({
+  origin: 'http://127.0.0.1:8080'
+}));
 
 app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
